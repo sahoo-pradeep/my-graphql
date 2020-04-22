4f5bfe35bf339a0b38eb590309ed1ef0ffd9dab2
Pradeep Kumar Sahoo <pradeep17sep@gmail.com>
2020-04-23 00:10:00 +0530

    added code

---
+++ src/test/java/projects/sahoo/mygraphql/MyGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.sahoo.mygraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
